4e8e40e85e79e1462d77caa550a380c7169a71c5
ttomas0520 <79412823+ttomas0520@users.noreply.github.com>
2023-07-17 15:57:12 +0200

    update contact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
-import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
- 
+import type { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	)
 }
- 
-export default MyApp;
+
+export default MyApp
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,72 @@
-import { Typography } from "@mui/material"
+import {
+	Box,
+	Card,
+	CardContent,
+	Container,
+	Grid,
+	Typography,
+} from "@mui/material"
 import ResponsiveAppBar from "../src/components/appbar"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import Link from "next/link"
 function Contact() {
 	return (
 		<>
 			<ResponsiveAppBar></ResponsiveAppBar>
-			<Typography>Kapcsolatok</Typography>
+			<Container>
+				<Card>
+					<CardContent
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						<Box>
+							<Typography
+								align="justify"
+								sx={{
+									fontSize: { xs: "14px", md: "24px", lg: "24px" },
+									textAlign: "center",
+								}}
+							>
+								NÁDASDY APARTMAN <br></br>
+								Magánszálláshely <br></br>
+								Cím: 3671 Borsodnádasd Karácsonylova utca 14. <br></br>
+								NTAK Regisztráció : MA23075465 <br></br>
+								Adószám:53030939-1-33<br></br>
+								Üzemeltető : Darcsi Irén Telefon:<br></br>
+								+36309254808 E-mail: nadasdyapartman@gmail.com<br></br>
+								<br></br>
+								Elérhetőek vagyunk az{" "}
+								<b>
+									<Link
+										href={
+											"https://www.facebook.com/profile.php?id=100094310367475"
+										}
+									>
+										Facebookon
+									</Link>
+									,{" "}
+									<Link
+										href={
+											"https://www.airbnb.hu/rooms/928157195653794227?check_in=2023-07-23&check_out=2023-07-28&guests=1&adults=1&s=67&unique_share_id=951dd94c-eb56-4b7d-8c28-1bf79094832c"
+										}
+									>
+										AirBnB
+									</Link>
+									,{" és "}
+									<Link href={"https://www.booking.com/Share-wQ82Lz"}>
+										Booking
+									</Link>{" "}
+									weblapokon is!
+								</b>
+							</Typography>
+						</Box>
+					</CardContent>
+				</Card>
+			</Container>
 		</>
 	)
 }
